a824c115f3fbae7fc8b49c2cfd1073719f1c4279
yeovolf <85723311+yeovolf@users.noreply.github.com>
2022-11-08 19:50:19 -0500

    first commit

---
+++ max_min_list.py
@@ -0,0 +1,18 @@
+# Giving list
+
+list1=[3, 4, 5, 6]
+list2=[-1, -2, -3, -4]
+list3=[0, 0, 0, 0]
+
+# empty list condition
+
+if len(list1) == 0 or len(list2) == 0 or len(list3) == 0:
+  print([])
+  print("none")
+  
+# print max and min number from the giving list
+
+else:
+  print(f"{max(list1)} {min(list1)}\n{max(list2)} {min(list2)}\n{max(list3)} {min(list3)}")
+
+  

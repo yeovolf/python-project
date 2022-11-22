c4b6b9a1f6fb1b8a98bd9053a2df5f6dce917988
yeovolf <85723311+yeovolf@users.noreply.github.com>
2022-11-22 18:44:09 -0500

    first commit

---
+++ password.py
@@ -0,0 +1,19 @@
+#This Program will first ask a user for a username and password using while loop
+
+credential = {
+  "username":"soho" ,
+  "password" : "Yeo"}
+username = ""
+password = ""
+n = 0       
+while n < 5:
+  username= str(input("enter your username\n")).lower()
+  password= str(input("enter your passwor\n")).lower()
+  if username != credential["username"]  or password != credential["password"]:
+    print("access denied try again")
+    n += 1
+  elif username == credential["username"] and password == credential["password"] :
+    print("you have access")
+    break
+  
+print("Your account is locked")    

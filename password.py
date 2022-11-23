64da4e7c0f7c9c9fe350c9441b94cbc5d13832d7
yeovolf <85723311+yeovolf@users.noreply.github.com>
2022-11-22 19:02:50 -0500

    Update password.py

--- password.py
+++ password.py
@@ -1,17 +1,23 @@
 #This Program will first ask a user for a username and password using while loop
 
-credential = {
-  "username":"soho" ,"password" : "Yeo"}
+credential = {"username":"soho" ,"password" : "Yeo"}
+
 username = ""
 password = ""
+
 count = 0 
 limite = 5
+
 while count < limit:
+  count += 1
+  
   username= str(input("enter your username\n")).lower()
   password= str(input("enter your passwor\n")).lower()
+  
   if username != credential["username"]  or password != credential["password"]:
     print("access denied try again")
-    count += 1
+    continue
+    
   elif username == credential["username"] and password == credential["password"] :
     print("you have access")
     break
